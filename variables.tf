1b93c99840bed3057a0bf08850532b2c9053664d
vladsha-dev <newshapovalov2002@gmail.com>
2025-01-28 13:06:29 +0100

    Downgrade the Postgres version to 16

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "allocated_storage" {
 }
 
 variable "engine_version" {
-  default     = "17"
+  default     = "16"
   type        = string
   description = "Database engine version"
 }
@@ -169,7 +169,7 @@ variable "subnet_group" {
 }
 
 variable "parameter_group" {
-  default     = "default.postgres17"
+  default     = "default.postgres16"
   type        = string
   description = "Database engine parameter group"
 }
